2662e15a4eae883fae42697acee682271c6c33cd
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-01-27 14:17:30 +0100

    Resize works

--- DevWar2/ResizeImage.cs
+++ DevWar2/ResizeImage.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace DevWar2
 {
@@ -8,18 +11,23 @@ namespace DevWar2
     {
         [FunctionName("ResizeImage")]
         public static void Run(
-            [BlobTrigger("photos/{name}", Connection = "StorageConnection")] Stream image,
+            [BlobTrigger("photos/{name}", Connection = "StorageConnection")] Stream inputStream,
             [Blob("resized-photos/{name}", FileAccess.Write, Connection = "StorageConnection")] Stream imageSmall,
             TraceWriter log)
         {
             log.Info("Resized");
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                image.CopyTo(ms);
-                var byteArray = ms.ToArray();
-                imageSmall.Write(byteArray, 0, byteArray.Length);
-            }
+            //using (MemoryStream ms = new MemoryStream())
+            //{
+            //    inputStream.CopyTo(ms);
+            //    var byteArray = ms.ToArray();
+            //    imageSmall.Write(byteArray, 0, byteArray.Length);
+            //}
+
+            Image<Rgba32> image = Image.Load(inputStream, new JpegDecoder());
+            image.Mutate(x => x.Resize(5,10));
+
+            image.Save(imageSmall, new JpegEncoder());
             //var imageBuilder = ImageResizer.ImageBuilder.Current;
             //var size = imageDimensionsTable[ImageSize.Small];
 
