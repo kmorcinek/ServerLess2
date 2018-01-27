0aa1d52cc860fe7d63f61ba578b3c7ef029178e0
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-01-27 12:50:28 +0100

    Change to Anonymous

--- DevWar2/HttpRetrieveOrder.cs
+++ DevWar2/HttpRetrieveOrder.cs
@@ -12,7 +12,7 @@ namespace DevWar2
     public static class HttpRetrieveOrder
     {
         [FunctionName("HttpRetrieveOrder")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req,
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req,
             [Table("Orders", Connection = "StorageConnection")]CloudTable ordersTable, TraceWriter log)
         {
             string fileName = req.Query["fileName"];
