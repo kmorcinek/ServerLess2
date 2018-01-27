3756469555c89be3dbfb6dfdf1a9f4b7e966561d
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-01-27 14:17:57 +0100

    Clean

--- DevWar2/ResizeImage.cs
+++ DevWar2/ResizeImage.cs
@@ -2,7 +2,6 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace DevWar2
@@ -17,28 +16,10 @@ namespace DevWar2
         {
             log.Info("Resized");
 
-            //using (MemoryStream ms = new MemoryStream())
-            //{
-            //    inputStream.CopyTo(ms);
-            //    var byteArray = ms.ToArray();
-            //    imageSmall.Write(byteArray, 0, byteArray.Length);
-            //}
-
             Image<Rgba32> image = Image.Load(inputStream, new JpegDecoder());
             image.Mutate(x => x.Resize(5,10));
 
             image.Save(imageSmall, new JpegEncoder());
-            //var imageBuilder = ImageResizer.ImageBuilder.Current;
-            //var size = imageDimensionsTable[ImageSize.Small];
-
-            //imageBuilder.Build(image, imageSmall,
-            //    new ResizeSettings(size.Item1, size.Item2, FitMode.Max, null), false);
-
-            //image.Position = 0;
-            //size = imageDimensionsTable[ImageSize.Medium];
-
-            //imageBuilder.Build(image, imageMedium,
-            //    new ResizeSettings(size.Item1, size.Item2, FitMode.Max, null), false);
         }
     }
 }
